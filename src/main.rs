77e4be376308cbe76309bdf27d1f62651cefc2e2
c1216440698 <c1216440698@gmail.com>
2026-01-13 19:47:20 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::{ sync::Arc };
-use polynomial::{ fetcher::{ DataEngine } };
+use polynomial::data_engine::DataEngine;
+use std::sync::Arc;
 mod common;
 use common::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
+    console_subscriber::init();
     let data_engine = Arc::new(DataEngine::new());
     data_engine.start();
-    tokio::signal::ctrl_c().await.expect("fail to listen to event");
+    tokio::signal::ctrl_c()
+        .await
+        .expect("fail to listen to event");
     Ok(())
 }
